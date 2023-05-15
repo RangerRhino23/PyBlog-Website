6a839cc62016ff92bf9958931590eeb7488fb3ba
Bailey <baileyleah542@gmail.com>
2023-05-15 15:41:22 +0100

    done a bit more

--- test.py
+++ test.py
@@ -4,9 +4,6 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route("/")
 def index():
-    variable = 'xyz'
-    return render_template('test.html', context = variable)
-
-
+    return render_template('test.html')
 
 app.run('localhost', 5000)
